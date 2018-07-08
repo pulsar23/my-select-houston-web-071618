b834721509d1f84362f015bb1cb7a6756a7785ea
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 23:32:37 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,5 @@
 def my_select(collection)
- i = 0
- new_array = []
- 
- while i < collection.length 
-    new_array[i] = yield(collection[i])
-    i+=1 
- end #while
- new_array
+
+
+
 end #my_select
