b69bfb79b98dc13e8c4c77a154f64a323076f251
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 23:34:11 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
-end
+
+ i = 0
+ j = 0
+ new_array = []
+ 
+ while i < collection.length 
+    value = yield(collection[i])
+    if value == true
+      new_array[j] = collection[i]
+      j+=1
+    end 
+    i+=1 
+ end #while
+ 
+ new_array
+ 
+end #my_select
+
